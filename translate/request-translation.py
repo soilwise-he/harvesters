c5bf423bfd09a3c5d53a36f3b1bfb9c634a31e35
Paul van Genuchten <genuchten@yahoo.com>
2024-10-15 10:30:03 +0200

    split extract and request

--- translate/request-translation.py
+++ translate/request-translation.py
@@ -1,6 +1,12 @@
-from rdflib import Graph, Literal, URIRef, term
-from rdflib.namespace import DC, DCTERMS, RDF, FOAF, SKOS
-import json, os, psycopg2
+# Desc: script triggers translation for prepared keys
+# Author: Paul van Genuchten
+# License: MIT
+
+import requests, json, os  
+from dotenv import load_dotenv
+import psycopg2
+from requests.auth import HTTPDigestAuth
+from datetime import datetime, timedelta
 
 import sys
 sys.path.append('utils')
@@ -10,116 +16,54 @@ from database import dbQuery, insertSQL
 from dotenv import load_dotenv
 load_dotenv()
 
-translatableProperties = [DCTERMS.title,DCTERMS.description]
-ISOPairs = {    
-    "bg": {"code":"bul","label":"Bulgarian"},        
-    "fr": {"code":"fre|fra","label":"French"},            
-    "pl": {"code":"pol","label":"Polish"},    
-    "cs": {"code":"cze|ces","label":"Czech"},            
-    "hr": {"code":"hrv","label":"Croatian"},            
-    "pt": {"code":"por","label":"Portuguese"},    
-    "da": {"code":"dan","label":"Danish"},        
-    "hu": {"code":"hun|mag","label":"Hungarian"},            
-    "ro": {"code":"rum|ron","label":"Romanian"},    
-    "de": {"code":"ger|deu","label":"German"},    
-    "is": {"code":"ice|isl","label":"Icelandic"},        
-    "ru": {"code":"rus","label":"Russian"},    
-    "et": {"code":"est","label":"Estonian"},        
-    "it": {"code":"ita","label":"Italian"},
-    "sk": {"code":"slo|slk","label":"Slovak"},    
-    "el": {"code":"gre|ell","label":"Greek"},    
-    "lt": {"code":"lit","label":"Lithuanian"},    
-    "sl": {"code":"slv","label":"Slovenian"},    
-    "es": {"code":"spa|esp","label":"Spanish"},    
-    "lv": {"code":"lav","label":"Latvian"},    
-    "sv": {"code":"swe","label":"Swedish"},    
-    "fi": {"code":"fin","label":"Finnish"},    
-    "nl": {"code":"dut|nld","label":"Dutch"},
-    "ch": {"code":"chi|zho", "label":"Chinese"},
-    "ar": {"code":"ara|arb", "label": "Arabic"}, 
-    "ja": {"code":"jap", "label": "Japanese"},
-    "ga": {"code":"gle", "label": "Irish"},
-    "nn": {"code":"nor", "label":"Norwegian"},
-    "tr": {"code":"tur", "label":"Turkish"}
-}
+eTranslationRestUrl = os.environ.get("TR_SERVICE_URL") or "https://webgate.ec.europa.eu/etranslation/si/translate"
+applicationName = os.environ.get("TR_LOGIN") or 'Soilwise-he'
+password = os.environ.get("TR_PASSCODE")
+callbackUrl = os.environ.get("TR_CALLBACK_URL")
 
-def isoMatch(lang):
-    for k,v in ISOPairs.items():
-        if lang.lower() == k or lang.lower() in v['code']:
-            return k
-    return None
+allowed_languages = os.environ.get("TR_ALLOWED_LANGUAGES") or '*'
 
+def requestRecord(lang_source,lang_target,source):
 
-def manageTrans(turtle,subject,prop,id,lang_source,lang_target="en"):
-    lang_source = isoMatch(lang_source)
-    if lang_source:
-        # first get the untranslated string
-        srctxt = None
-        for s,p,o in turtle.triples((subject,prop,None)):
-            if o.__class__ == term.Literal:
-                srctxt = str(o) # use any language
-                if isoMatch(o.language) == lang_source: # if default language
-                    srctxt = str(o)
-                    break
-            else:
-                print (f'No term {o}')
+    translationRequest = {}
+    translationRequest['sourceLanguage'] = lang_source
+    translationRequest['targetLanguages'] = [lang_target]
+    translationRequest['callerInformation'] = {"application" : applicationName, "username":"ingest-bot"}
+    translationRequest['textToTranslate'] = source
+    translationRequest['requesterCallback'] = callbackUrl
 
-        if not srctxt:
-            print('No source for {prop}:{lang_source}')
-        else:
-            # find a english translation
-            hasENG = False
-            for s,p,o in turtle.triples((subject,prop,None)):
-                if o.language == 'en':
-                    hasENG = True
-                    # insert trans
-                    insertSQL('harvest.translations',
-                            ['source','target','lang_source','lang_target','context'],
-                            (srctxt,o,lang_source,lang_target,prop))
-                    break
+    jsonTranslationRequest = json.dumps(translationRequest)
+    jsonHeader = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+    requestId = None
+
+    try:
+        response = requests.post(eTranslationRestUrl, auth=HTTPDigestAuth(applicationName, password), headers = jsonHeader, data=jsonTranslationRequest)  
+        response.raise_for_status()
+        requestId = response.text
+    except requests.exceptions.HTTPError as err:
+        print(err)
         
-            # prepare a translation for this record
-            if not hasENG:
-                # insert untranslated
-                insertSQL('harvest.translations',
-                        ['source','lang_source','lang_target','context'],
-                        (srctxt,lang_source,lang_target,id))
+    print(response.text)
 
-
-
-# get records which have language<>en and are not already translated
-sql = '''select identifier,language,turtle from harvest.items 
-    where not coalesce(language,'') = ''
-    and not coalesce(turtle,'') = ''
-    and not upper(language) = any(ARRAY['EN','ENG']) 
-    and identifier not in (select coalesce(context,'') from harvest.translations) 
-    limit 2500'''
-
-recs = dbQuery(sql)
-
-if recs:
-    total = len(recs)
-    counter = 0
-    print(total)
+    if(requestId and requestId not in [None,'']):
+        # insert the ticket to the database, to identify which string to be updated
+        dbQuery("update harvest.translations set ticket=%s where lang_source=%s and lang_target=%s and source=%s;",(requestId,lang_source,lang_target,source),hasoutput=False)
+        return requestId
+    else:
+        return "Error: No requestId"
     
-    for rec in sorted(recs):
-        identifier,language,turtle = rec
-
-        g = Graph()
-        g.parse(data=turtle, format='turtle')
+def main():
+    # query database for pending translations
+    myrecs = dbQuery("""select source,lang_source,lang_target 
+                     from harvest.translations 
+                     where target is null
+                     and (ticket is null or date_inserted < now() - interval '1 day')""")
+    # for each string to be translated, make a request
+    for rec in myrecs:
+        source,lang_source,lang_target = rec 
+        requestRecord(lang_source,lang_target,source)
         
-        subject = None
-        # combination of language and description are the objects we're interested in
-        for s,p,o in g.triples((None,DCTERMS.description,None)):
-            if (s, DCTERMS.language, None) in g:
-                subject = s
-                break
-    
-        if subject:
-            if isoMatch(language) not in ['None','en','']:
-                print(f"translations for {identifier} in {isoMatch(language)}")
-                for p in translatableProperties:
-                    manageTrans(g,subject,p,identifier,language)
-        else:
-            print('no subject')
+    return "OK"
 
+if __name__ == "__main__":
+    main()
