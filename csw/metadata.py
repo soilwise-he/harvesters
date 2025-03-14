acdc1937727d8a8b0d52604da9ba8027438ca607
Paul van Genuchten <genuchten@yahoo.com>
2025-03-14 11:56:44 +0100

    add extra try catch if owslib fails a step, lower pagination to get more results

--- csw/metadata.py
+++ csw/metadata.py
@@ -57,67 +57,67 @@ if filters and len(filters) > 0:
             constraints.append(PropertyIsEqualTo(key, v))
 
 while nextRecord > 0 and returned > 0 and nextRecord < matched and nextRecord < maxrecords:
+    try:
+        if len(constraints) > 0:
+            csw.getrecords2(maxrecords=pagesize,outputschema='http://www.isotc211.org/2005/gmd',constraints=constraints,startposition=nextRecord,esn='full')
+        else:
+            csw.getrecords2(maxrecords=pagesize,outputschema='http://www.isotc211.org/2005/gmd',startposition=nextRecord,esn='full')
+        
+        print('CSW query ' + str(csw.results['returned']) + ' of ' + str(csw.results['matches']) + ' records from ' + str(nextRecord) + '.')
+        matched = csw.results['matches']
+        nextRecord = csw.results['nextrecord'] or (nextRecord+pagesize)
+        returned = csw.results['returned'] or len(csw.records)
+        
+        for rec in csw.records:
+            recxml = csw.records[rec].xml
+            try:
+                if 'GetRecordByIdResponse' in str(recxml):
+                    try:
+                        doc = etree.fromstring(recxml)
+                    except ValueError:
+                        print(f'iso19139 parse failed {u}')
+                        doc = etree.fromstring(bytes(recxml, 'utf-8'))
+                    nsmap = {}
+                    for ns in doc.xpath('//namespace::*'):
+                        if ns[0]:
+                            nsmap[ns[0]] = ns[1]
 
-    if len(constraints) > 0:
-        csw.getrecords2(maxrecords=pagesize,outputschema='http://www.isotc211.org/2005/gmd',constraints=constraints,startposition=nextRecord,esn='full')
-    else:
-        csw.getrecords2(maxrecords=pagesize,outputschema='http://www.isotc211.org/2005/gmd',startposition=nextRecord,esn='full')
-    
-    
-    print('CSW query ' + str(csw.results['returned']) + ' of ' + str(csw.results['matches']) + ' records from ' + str(nextRecord) + '.')
-    matched = csw.results['matches']
-    nextRecord = csw.results['nextrecord'] or (nextRecord+pagesize)
-    returned = csw.results['returned'] or len(csw.records)
-    
-    for rec in csw.records:
-        recxml = csw.records[rec].xml
-        try:
-            if 'GetRecordByIdResponse' in str(recxml):
-                try:
-                    doc = etree.fromstring(recxml)
-                except ValueError:
-                    print(f'iso19139 parse failed {u}')
-                    doc = etree.fromstring(bytes(recxml, 'utf-8'))
-                nsmap = {}
-                for ns in doc.xpath('//namespace::*'):
-                    if ns[0]:
-                        nsmap[ns[0]] = ns[1]
+                    rec2 = doc.xpath('gmd:MD_Metadata', namespaces=nsmap)
+                    recxml = etree.tostring(rec2[0])
 
-                rec2 = doc.xpath('gmd:MD_Metadata', namespaces=nsmap)
-                recxml = etree.tostring(rec2[0])
+                hashcode = hashlib.md5(recxml).hexdigest() # get unique hash for xml 
 
-            hashcode = hashlib.md5(recxml).hexdigest() # get unique hash for xml 
+                id=''
+                identifier=str(uuid.uuid4())
+                hierarchy=''
+                try:            
+                    m=MD_Metadata(etree.fromstring(recxml))
+                    id = m.dataseturi
+                    if id in [None,'']:
+                        for i in m.identification:
+                            for i2 in i.uricode:
+                                if i2 not in [None,'']:
+                                    id = i2
+                    if id in [None,'']:
+                        id = m.identifier
+                    identifier = m.identifier
+                    hierarchy = m.hierarchy
+                except Exception as e:
+                    print(f'Failed parse xml: {str(e)} {str(sys.exc_info())}')
 
-            id=''
-            identifier=str(uuid.uuid4())
-            hierarchy=''
-            try:            
-                m=MD_Metadata(etree.fromstring(recxml))
-                id = m.dataseturi
-                if id in [None,'']:
-                    for i in m.identification:
-                        for i2 in i.uricode:
-                            if i2 not in [None,'']:
-                                id = i2
-                if id in [None,'']:
-                    id = m.identifier
-                identifier = m.identifier
-                hierarchy = m.hierarchy
+                insertRecord(       identifier=id,
+                                    uri=identifier,
+                                    identifiertype='uuid',
+                                    resulttype='iso19139:2007',
+                                    resultobject=recxml.decode('UTF-8'),
+                                    hashcode=hashcode,
+                                    source=label,
+                                    itemtype=hierarchy) # insert into db
+                
             except Exception as e:
-                print(f'Failed parse xml: {str(e)} {str(sys.exc_info())}')
-
-            insertRecord(       identifier=id,
-                                uri=identifier,
-                                identifiertype='uuid',
-                                resulttype='iso19139:2007',
-                                resultobject=recxml.decode('UTF-8'),
-                                hashcode=hashcode,
-                                source=label,
-                                itemtype=hierarchy) # insert into db
-            
-        except Exception as e:
-            print(f"Parse rec failed; {str(e)}")
-
+                print(f"Parse rec failed; {str(e)}")
+    except Exception as e:
+        print(f"Request recs failed; {str(e)}")
 
 
 
